7cc06877a6a4a1bed0b0180c0547401de335f01c
Edmndsn <edmondson.jackw@gmail.com>
2022-07-19 20:17:42 +0100

    imported icons for nav links

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,25 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import dashboardIcon from "../Images/dashboard-icon.png";
+import expensesIcon from "../Images/expenses-icon.png";
+import settingsIcon from "../Images/settings-icon.png";
+
 import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav>
       <div className="nav-links">
-        <NavLink to="/" className="navlink-dashboard">Dashboard</NavLink>
-        <NavLink to="Expenses" className="navlink-expenses">Expenses</NavLink>
-        <NavLink to="Contact" className="navlink-contact">Dashboard</NavLink>
+        <NavLink to="/" className="navlink-dashboard">
+          <img src={dashboardIcon} />
+          Dashboard
+        </NavLink>
+        <NavLink to="Expenses" className="navlink-expenses">
+        <img src={expensesIcon} />Expenses
+        </NavLink>
+        <NavLink to="Settings" className="navlink-settings">
+        <img src={settingsIcon} /> Dashboard
+        </NavLink>
       </div>
     </nav>
   );
