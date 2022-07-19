0c4739ed6aa9e272a270a136e93ce740ac7c00a5
Edmndsn <edmondson.jackw@gmail.com>
2022-07-19 22:23:13 +0100

    changed images from png to svg, updated CSS

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./Components/Navbar/Navbar";
 import Dashboard from "./Components/Dashboard/Dashboard";
 import Expenses from "./Components/Expenses/Expenses";
 import Settings from "./Components/Settings/Settings";
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes } from "react-router-dom";
 
 export default function App() {
   return (
@@ -11,8 +11,9 @@ export default function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Dashboard />} />
-        <Route path="Expenses" element={<Expenses />} />
-        <Route path="Settings" element={<Settings />} />
+        <Route path="expenses" element={<Expenses />} />
+        <Route path="settings" element={<Settings />} />
+        
       </Routes>
     </div>
   );
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,14 +1,16 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import dashboardIcon from "../Images/dashboard-icon.png";
-import expensesIcon from "../Images/expenses-icon.png";
-import settingsIcon from "../Images/settings-icon.png";
+import dashboardIcon from "../Images/dashboard-icon.svg";
+import expensesIcon from "../Images/expenses-icon.svg";
+import settingsIcon from "../Images/settings-icon.svg";
+import Logo from '../Images/Logo.svg'
 
 import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav>
+        <img className="logo" src={Logo} />
       <div className="nav-links">
         <NavLink to="/" className="navlink-dashboard">
           <img src={dashboardIcon} />
@@ -18,7 +20,7 @@ export default function Navbar() {
         <img src={expensesIcon} />Expenses
         </NavLink>
         <NavLink to="Settings" className="navlink-settings">
-        <img src={settingsIcon} /> Dashboard
+        <img src={settingsIcon} /> Settings
         </NavLink>
       </div>
     </nav>
