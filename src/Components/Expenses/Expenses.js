99ee0134747cc5c2cbf8b25a1528af51791cadd4
Edmndsn <edmondson.jackw@gmail.com>
2022-07-20 14:59:17 +0100

    made a start on the expenses/transactions component page. Added search bar input and styled

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,7 +1,21 @@
-import React from 'react'
+import React from "react";
+import searchIcon from "../Images/search-icon.svg";
+import "./Expenses.css";
 
 export default function Expenses() {
   return (
-    <div>Expenses</div>
-  )
+    <div container="expenses-container">
+      <div className="expenses-content">
+        <h2 className="expenses-title">Expenses</h2>
+        <div className="search-bar-container">
+          <img src={searchIcon} />
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search for specific transactions"
+          ></input>
+        </div>
+      </div>
+    </div>
+  );
 }
