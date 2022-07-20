88971d26d6c13c7718eaebbeb9f7342226345334
Edmndsn <edmondson.jackw@gmail.com>
2022-07-20 10:05:08 +0100

    added logout button and nested logout icon image within

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -3,26 +3,30 @@ import { NavLink } from "react-router-dom";
 import dashboardIcon from "../Images/dashboard-icon.svg";
 import expensesIcon from "../Images/expenses-icon.svg";
 import settingsIcon from "../Images/settings-icon.svg";
-import Logo from '../Images/Logo.svg'
+import Logo from "../Images/Logo.svg";
+import logoutIcon from '../Images/logout-icon.svg';
 
 import "./Navbar.css";
 
 export default function Navbar() {
   return (
     <nav>
-        <img className="logo" src={Logo} />
+      <img className="logo" src={Logo} />
       <div className="nav-links">
         <NavLink to="/" className="navlink-dashboard">
           <img src={dashboardIcon} />
           Dashboard
         </NavLink>
         <NavLink to="Expenses" className="navlink-expenses">
-        <img src={expensesIcon} />Expenses
+          <img src={expensesIcon} />
+          Expenses
         </NavLink>
         <NavLink to="Settings" className="navlink-settings">
-        <img src={settingsIcon} /> Settings
+          <img src={settingsIcon} /> Settings
         </NavLink>
       </div>
+      <button className="logout-button"><img src={logoutIcon}/>
+      Logout</button>
     </nav>
   );
 }
