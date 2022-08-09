6b691c7ae29595b17b3e8236422f038bc634b00c
Edmndsn <edmondson.jackw@gmail.com>
2022-08-09 16:56:29 +0100

    added base to ExpensesContext

--- src/Components/Settings/Settings.js
+++ src/Components/Settings/Settings.js
@@ -1,47 +1,49 @@
-import React from 'react'
-import { useAuth } from '../../Contexts/auth';
-import Pen from '../../images/Edit_icon.svg'
-import Mail from '../../images/Email_icon.svg'
-import Lock from '../../images/Lock_icon.svg'
-import Eye from '../../images/Eye_icon.svg'
-import './settings.css'
-import { updateProfile, updateEmail,
-         updatePassword, EmailAuthProvider , 
-         reauthenticateWithCredential} from 'firebase/auth';
+import React from "react";
+import { useAuth } from "../../Contexts/auth";
+import Pen from "../../images/Edit_icon.svg";
+import Mail from "../../images/Email_icon.svg";
+import Lock from "../../images/Lock_icon.svg";
+import Eye from "../../images/Eye_icon.svg";
+import "./settings.css";
+import {
+  updateProfile,
+  updateEmail,
+  updatePassword,
+  EmailAuthProvider,
+  reauthenticateWithCredential,
+} from "firebase/auth";
 
 export default function Settings() {
-
-  const {
-    currentUser,
-  } = useAuth();
+  const { currentUser } = useAuth();
 
   const [userData, setUserData] = React.useState({
     name: currentUser.displayName,
-    email: currentUser.email || '',
-    phoneNumber: currentUser.phoneNumber || '',
-    photo: currentUser.photoUrl || '',
-    password: '',
-    passwordConfirmation: '',
-
+    email: currentUser.email || "",
+    phoneNumber: currentUser.phoneNumber || "",
+    photo: currentUser.photoUrl || "",
+    password: "",
+    passwordConfirmation: "",
   });
 
-  console.log(currentUser)
+  console.log(currentUser);
 
   const [error, setError] = React.useState({
-    name: '',
-    email: '',
-    phone: '',
-    password: '',
-    passwordConfirmation: '',
+    name: "",
+    email: "",
+    phone: "",
+    password: "",
+    passwordConfirmation: "",
   });
 
   const [edit, setEdit] = React.useState(false);
 
   const [firstName, setFirstName] = React.useState(
-    userData.name.substring(0, userData.name.indexOf(' ')));
-  
+    userData.name.substring(0, userData.name.indexOf(" "))
+  );
+
   const [lastName, setLastName] = React.useState(
-    userData.name.substring(userData.name.indexOf(' ') + 1));
+    userData.name.substring(userData.name.indexOf(" ") + 1)
+  );
 
   const [userBirth, setUserBirth] = React.useState();
 
@@ -50,7 +52,6 @@ export default function Settings() {
   const [userPassword, setUserPassword] = React.useState();
 
   const [passwordShown, setPasswordShown] = React.useState(false);
-
 
   // const handleChange = (e) => {
   //   const {name, value} = e.target;
@@ -63,53 +64,59 @@ export default function Settings() {
   //   })
   // };
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async e => {
     e.preventDefault();
-    const {name, email, phoneNumber, photo, password, passwordConfirmation} = userData;
+    const { name, email, phoneNumber, photo, password, passwordConfirmation } =
+      userData;
 
     const credentials = EmailAuthProvider.credential(
       email,
       passwordConfirmation,
       userData.uid
-    )
+    );
 
-    if (currentUser.email !== email ) {
-      
+    if (currentUser.email !== email) {
       console.log(userData);
 
-      // console.log(credentials); 
+      // console.log(credentials);
 
-      reauthenticateWithCredential(email, credentials).then(()=> {
+      reauthenticateWithCredential(email, credentials).then(() => {
         updateEmail(currentUser, { email })
-          .then(()=> {
-            alert('Email updated successfully')
-            })
-          .catch((error)=> {
+          .then(() => {
+            alert("Email updated successfully");
+          })
+          .catch(error => {
             console.log(error);
-            });
-      })
+          });
+      });
     }
 
     if (currentUser.displayName !== `${firstName} ${lastName}`) {
       updateProfile(currentUser, {
         displayName: `${firstName} ${lastName}`,
       })
-      .then(()=> { alert('Name updated successfully') })
-      .catch((error)=> { console.log(error) });
+        .then(() => {
+          alert("Name updated successfully");
+        })
+        .catch(error => {
+          console.log(error);
+        });
     }
 
-    if (userPassword){
-      console.log(credentials)
+    if (userPassword) {
+      console.log(credentials);
 
-      reauthenticateWithCredential(currentUser, credentials).then(()=> {
+      reauthenticateWithCredential(currentUser, credentials).then(() => {
         updatePassword(currentUser, { userPassword })
-        .then(()=> {
-          alert('Password updated successfully')
+          .then(() => {
+            alert("Password updated successfully");
           })
-        .catch((error)=> { console.log(error) })
-      })
-    } 
-  }
+          .catch(error => {
+            console.log(error);
+          });
+      });
+    }
+  };
 
   return (
     <div className="settings-container">
@@ -125,131 +132,146 @@ export default function Settings() {
         <h2 className="subtitle-acc">Account Information</h2>
         <p className="subtitle-details">Update your account information</p>
 
-      <div className='main-container'>
-        <h2 className='subtitle-acc'>Account Information</h2>
-        <p className='subtitle-details'>Update your account information</p>
+        <div className="main-container">
+          <h2 className="subtitle-acc">Account Information</h2>
+          <p className="subtitle-details">Update your account information</p>
 
-        <section className='personal-info'>
-          <h2 className='subtitle-pers'>Personal Information</h2>
-          <button className='edit-btn' onClick={() => setEdit(true)}> 
-            <img src={Pen} className='pen-icon' alt=''/>
-            Edit
-          </button>
-        </section>
+          <section className="personal-info">
+            <h2 className="subtitle-pers">Personal Information</h2>
+            <button className="edit-btn" onClick={() => setEdit(true)}>
+              <img src={Pen} className="pen-icon" alt="" />
+              Edit
+            </button>
+          </section>
 
-        
-        <form className='form-info'>
-          <section className='form-wrap'>
-            <div className='form-column'>
-              <label className='label label-fname'>First Name</label>
-              <input 
+          <form className="form-info">
+            <section className="form-wrap">
+              <div className="form-column">
+                <label className="label label-fname">First Name</label>
+                <input
+                  disabled={!edit}
+                  className="input input-fname"
+                  type="text"
+                  name="firstname"
+                  onChange={event => {
+                    setFirstName(event.target.value);
+                  }}
+                  placeholder={firstName}
+                />
+              </div>
+
+              <div className="form-column">
+                <label className="label label-lname">Last Name</label>
+                <input
+                  disabled={!edit}
+                  className="input input-lname"
+                  type="text"
+                  name="lastName"
+                  onChange={event => {
+                    setLastName(event.target.value);
+                  }}
+                  placeholder={lastName}
+                />
+              </div>
+
+              <div className="form-column">
+                <label className="label label-date">Date of birth</label>
+                <input
+                  disabled={!edit}
+                  className="input input-date"
+                  type="date"
+                  name="dob"
+                  onChange={event => {
+                    setUserBirth(event.target.value);
+                  }}
+                  placeholder={userBirth}
+                />
+              </div>
+
+              <div className="form-column">
+                <label className="label label-phone">Mobile Number</label>
+                <input
+                  disabled={!edit}
+                  className="input input-phone"
+                  type="number"
+                  name="phone"
+                  onChange={event => {
+                    setUserPhone(event.target.value);
+                  }}
+                  placeholder={userData.phoneNumber}
+                />
+              </div>
+            </section>
+
+            <label className="label-email">Email</label>
+            <div className="mail-container">
+              <img src={Mail} className="mail-icon" alt="" />
+              <input
                 disabled={!edit}
-                className='input input-fname' 
-                type="text" 
-                name="firstname" 
-                onChange={(event) => {setFirstName(event.target.value)}}
-                placeholder={firstName}
+                className="input-mail"
+                type="email"
+                name="email"
+                onChange={event => {
+                  setUserEmail(event.target.value);
+                }}
+                placeholder={userData.email}
               />
             </div>
 
-            <div className='form-column'>
-            <label className='label label-lname'>Last Name</label>
-            <input
-              disabled={!edit}
-              className='input input-lname' 
-              type="text"
-              name="lastName" 
-              onChange={(event) => {setLastName(event.target.value)}}
-              placeholder={lastName}
-            />
-            </div>
-
-            <div className='form-column'>
-            <label className='label label-date'>Date of birth</label>
-            <input
-              disabled={!edit}
-              className='input input-date'
-              type="date"
-              name="dob"
-              onChange={(event) => {setUserBirth(event.target.value)}}
-              placeholder={userBirth}
-            />
-            </div>
-
-            <div className='form-column'>
-            <label className='label label-phone'>Mobile Number</label>
-            <input
-              disabled={!edit} 
-              className='input input-phone'
-              type="number"
-              name="phone"
-              onChange={(event) => {setUserPhone(event.target.value)}}
-              placeholder={userData.phoneNumber}
-            />
-            </div>
-          </section>
-
-          <label className="label-email">Email</label>
-          <div className="mail-container">
-            <img src={Mail} className="mail-icon" alt="" />
-            <input
-              disabled={!edit}
-              className='input-mail'
-              type="email"
-              name="email"
-              onChange={(event) => {setUserEmail(event.target.value)}}
-              placeholder={userData.email}
-            />
-          </div>
-
-          <section className='form-wrap'>
-            <div className='form-column'>
-              <label className='label-pass'>New Password</label>
-              <div className='pass-container'>
-                <img src={Lock} className='lock-icon' alt=''/>
-                <input 
-                  disabled={!edit}
-                  className='pass-input' 
-                  type={passwordShown ? "text" : "password"} 
-                  name="password" 
-                  onChange={(event) => {
-                    event.preventDefault();
-                    setUserPassword(event.target.value)}}
-                  placeholder='·······'
-                />
-                <button onClick={() => setPasswordShown(true)}>
-                  <img src={Eye} className='eye-icon' alt=''/>
-                </button>
+            <section className="form-wrap">
+              <div className="form-column">
+                <label className="label-pass">New Password</label>
+                <div className="pass-container">
+                  <img src={Lock} className="lock-icon" alt="" />
+                  <input
+                    disabled={!edit}
+                    className="pass-input"
+                    type={passwordShown ? "text" : "password"}
+                    name="password"
+                    onChange={event => {
+                      event.preventDefault();
+                      setUserPassword(event.target.value);
+                    }}
+                    placeholder="·······"
+                  />
+                  <button onClick={() => setPasswordShown(true)}>
+                    <img src={Eye} className="eye-icon" alt="" />
+                  </button>
+                </div>
               </div>
-            </div>
 
-            <div className='form-column'>
-              <label className='label-pass'>Confirm Password</label>
-              <div className='pass-container'>
-                <img src={Lock} className='lock-icon' alt=''/>
-                <input 
-                  disabled={!edit}
-                  className='pass-input' 
-                  type={passwordShown ? "text" : "password"}
-                  name="password" 
-                  onChange={(event) => {
-                    event.preventDefault();
-                    setUserPassword(event.target.value)}}
-                  placeholder='·······'
-                />
-                <button onClick={() => setPasswordShown(true)}>
-                  <img src={Eye} className='eye-icon' alt=''/>
-                </button>
+              <div className="form-column">
+                <label className="label-pass">Confirm Password</label>
+                <div className="pass-container">
+                  <img src={Lock} className="lock-icon" alt="" />
+                  <input
+                    disabled={!edit}
+                    className="pass-input"
+                    type={passwordShown ? "text" : "password"}
+                    name="password"
+                    onChange={event => {
+                      event.preventDefault();
+                      setUserPassword(event.target.value);
+                    }}
+                    placeholder="·······"
+                  />
+                  <button onClick={() => setPasswordShown(true)}>
+                    <img src={Eye} className="eye-icon" alt="" />
+                  </button>
+                </div>
+                <p>{error.password}</p>
               </div>
-              <p>{error.password}</p>
-            </div>
-          </section>
+            </section>
 
-          <button 
-            disabled={!edit}
-            onClick={handleSubmit}
-            className='update-btn'>Update</button>
-        </form>
+            <button
+              disabled={!edit}
+              onClick={handleSubmit}
+              className="update-btn"
+            >
+              Update
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
---
+++ src/Contexts/ExpensesContext.js
@@ -0,0 +1,64 @@
+import {
+  collection,
+  doc,
+  getDocs,
+  updateDoc,
+  deleteDoc,
+  setDoc,
+  onSnapshot,
+  query,
+  orderBy,
+  getDoc,
+} from "firebase/firestore";
+
+import { createContext, useContext, useState, useEffect } from "react";
+
+import { db } from "../firebase-config";
+
+import { useAuth } from "./auth";
+
+const ExpensesContext = createContext();
+
+export function useExpenses() {
+  return useContext(ExpensesContext);
+}
+
+export default function ExpensesProvider({ children }) {
+  const { currentUser } = useAuth();
+  const uid = currentUser && currentUser.uid;
+  const [expenses, setExpenses] = useState([]);
+  const expensesRef = currentUser && collection(db, "users", uid, "expense");
+  const displayQ = query(expensesRef, orderBy("date", "desc"));
+
+  useEffect(() => {
+    if (currentUser) {
+      onSnapshot(expensesRef, async () => {
+        const data = await getDocs(displayQ);
+        const expensesArray = data.docs.map(doc => doc.data());
+        setExpenses(expensesArray);
+      });
+    }
+  }, [currentUser]);
+
+  function createExpense(id, data) {
+    return setDoc(doc(db, "users", uid, "expense", id), data);
+  }
+  function updateExpense(id, data) {
+    return updateDoc(doc(db, "users", uid, "expense", id), data);
+  }
+  function deleteExpense(id, data) {
+    return deleteDoc(doc(db, "users", uid, "expense", id), data);
+  }
+  const value = {
+    expenses,
+    createExpense,
+    updateExpense,
+    deleteExpense,
+  };
+
+  return (
+    <ExpensesContext.Provider value={value}>
+      {children}
+    </ExpensesContext.Provider>
+  );
+}
