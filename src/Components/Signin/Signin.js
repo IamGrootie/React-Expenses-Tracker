8763627a11a26d37629bcf027cda7a9341ad7dcb
Jack Edmondson <99421203+Edmndsn@users.noreply.github.com>
2022-08-16 16:29:51 +0100

    updated styling for Signup/Signin

--- src/Components/Signin/Signin.js
+++ src/Components/Signin/Signin.js
@@ -1,132 +1,149 @@
-import React from 'react';
-import {useNavigate, Link} from 'react-router-dom';
-import {signInWithEmailAndPassword, setPersistence, browserSessionPersistence, onAuthStateChanged} from 'firebase/auth';
+import React from "react";
+import { useNavigate, Link } from "react-router-dom";
+import {
+	signInWithEmailAndPassword,
+	setPersistence,
+	browserSessionPersistence,
+	onAuthStateChanged,
+} from "firebase/auth";
 import { signInWithPopup } from "firebase/auth";
 import { auth, provider } from "../../firebase-config";
-import {useAuth} from "../../Contexts/RequireAuth.js";
-import Logo from '../../images/Logo.svg'
-import google from '../../images/Google.svg'
-import vector from '../../images/Vector.svg'
-import Main from '../../images/Intro_img.svg'
-import './signin.css'
+import { useAuth } from "../../Contexts/RequireAuth.js";
+import Logo from "../../images/Logo.svg";
+import google from "../../images/Google.svg";
+import vector from "../../images/Vector.svg";
+import Main from "../../images/Intro_img.svg";
+import "./signin.css";
 
 export default function Signin() {
-    const [user, setUser] = React.useState({});
-    const navigate = useNavigate();
+	const [user, setUser] = React.useState({});
+	const navigate = useNavigate();
 
-    const [userLogin, setUserLogin] = React.useState({
-        email: '',
-        password: ''
-    });
+	const [userLogin, setUserLogin] = React.useState({
+		email: "",
+		password: "",
+	});
 
-    React.useEffect(() => {
-        onAuthStateChanged(auth, (currentUser) => {
-            setUser(currentUser);
-        });
-    }, []);
+	React.useEffect(() => {
+		onAuthStateChanged(auth, (currentUser) => {
+			setUser(currentUser);
+		});
+	}, []);
 
-    function handleSubmit(e) {
-        e.preventDefault();
-                console.log(userLogin.email,userLogin.password) //Attention change this
-                
-        signInWithEmailAndPassword(auth, userLogin.email, userLogin.password)
-          .then((cred)=>{console.log("user logined:"+cred.user)
-          sessionStorage.setItem("Auth Token", auth.currentUser.accessToken);
-          sessionStorage.setItem("uid", auth.currentUser.uid);
-          sessionStorage.setItem("email", auth.currentUser.email);
-          navigate("/")} )
-          .catch((err) => { console.log(err) })
-    }
+	function handleSubmit(e) {
+		e.preventDefault();
+		console.log(userLogin.email, userLogin.password); //Attention change this
 
-    function handleChange(e) {
-        console.log("change")
-        const { type, value } = e.target;
-        setUserLogin((prev) => ({ ...prev, [type]: value }))
-      }
+		signInWithEmailAndPassword(auth, userLogin.email, userLogin.password)
+			.then((cred) => {
+				console.log("user logined:" + cred.user);
+				sessionStorage.setItem("Auth Token", auth.currentUser.accessToken);
+				sessionStorage.setItem("uid", auth.currentUser.uid);
+				sessionStorage.setItem("email", auth.currentUser.email);
+				navigate("/");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
+	function handleChange(e) {
+		console.log("change");
+		const { type, value } = e.target;
+		setUserLogin((prev) => ({ ...prev, [type]: value }));
+	}
 
-    function handlePersistance(e){
-        setPersistence(auth, browserSessionPersistence)
-        .then((email, password) => {
-            return signInWithEmailAndPassword(auth, email, password);
-        })
-        .catch((error) => {
-            // Handle Errors here.
-            const errorCode = error.code;
-            const errorMessage = error.message;
-        });
-    }
+	function handlePersistance(e) {
+		setPersistence(auth, browserSessionPersistence)
+			.then((email, password) => {
+				return signInWithEmailAndPassword(auth, email, password);
+			})
+			.catch((error) => {
+				// Handle Errors here.
+				const errorCode = error.code;
+				const errorMessage = error.message;
+			});
+	}
 
-    function handleGoogle(e){
-        e.preventDefault();
-                console.log(userLogin.email,userLogin.password)
-        signInWithPopup(auth, provider)
-        .then((cred)=>{
-            console.log(cred);
-            sessionStorage.setItem("Auth Token", auth.currentUser.accessToken);
-            sessionStorage.setItem("uid", auth.currentUser.uid);
-            sessionStorage.setItem("email", auth.currentUser.email);
-            navigate("/")
-        })
-        .catch((err) => { console.log(err) })
-    }
+	function handleGoogle(e) {
+		e.preventDefault();
+		console.log(userLogin.email, userLogin.password);
+		signInWithPopup(auth, provider)
+			.then((cred) => {
+				console.log(cred);
+				sessionStorage.setItem("Auth Token", auth.currentUser.accessToken);
+				sessionStorage.setItem("uid", auth.currentUser.uid);
+				sessionStorage.setItem("email", auth.currentUser.email);
+				navigate("/");
+			})
+			.catch((err) => {
+				console.log(err);
+			});
+	}
 
-    const {forgotPassword} = useAuth();
+	const { forgotPassword } = useAuth();
 
-    const forgotPasswordHandler = () => {
-        console.log(userLogin.email)
-        const email = userLogin.email;
-        if (email)
-          forgotPassword(email).then(() => {
-            userLogin.email = "";
-          });
-      };
+	const forgotPasswordHandler = () => {
+		console.log(userLogin.email);
+		const email = userLogin.email;
+		if (email)
+			forgotPassword(email).then(() => {
+				userLogin.email = "";
+			});
+	};
 
-  return (
-    <div className='intro-container'>
-        <div className='form-container'>
-            <img src={Logo} className='logo' alt=''/>
-            <h1 className='title-sign'>Welcome back</h1>
-            <p className='details'>Welcome back! Please enter your details</p>
-            <form className='form-signup'>
-                <label className='email-label'>Email</label>
-                <input 
-                    className='details-input' 
-                    type="email" 
-                    name="email" 
-                    onChange={handleChange} 
-                    placeholder='Enter your email address'
-                />
-                <label className='password-label'>Password</label>
-                <input 
-                    className='details-input' 
-                    type="password" 
-                    name="password" 
-                    onChange={handleChange}
-                    placeholder='·······'/>
-                <section className='options-section'>
-                    <div>
-                        <input className='remember-input' type='checkbox' onChange={handlePersistance} />
-                        <label className='remember-label'>Remember for 30 Days</label>
-                    </div>
-                    <p onClick={forgotPasswordHandler}>Forgot password</p>
-                </section>
-                <button 
-                    onClick={handleSubmit}
-                    className='sign-btn'>
-                        Sign in
-                </button>
-                <button 
-                    onClick={handleGoogle}  
-                    className='google-btn'>
-                    <img src={google} alt=''/>
-                    Sign in with google
-                </button>
-                <p className='question'>Don't have an account? <Link to="/signup">Sign up for free</Link></p>
-                <img src={vector} className='vector' alt=''/>
-            </form>
-        </div>
-        <img src={Main} alt=''/>
-    </div>
-  )
+	return (
+		<div className="intro-container">
+			<div className="form-half">
+				<div className="form-container">
+					<img src={Logo} className="logo" alt="" />
+					<h1 className="title-sign">Welcome back!</h1>
+					<p className="details">Please enter your details.</p>
+					<form className="form-signup">
+						<label className="email-label">Email</label>
+						<input
+							className="details-input"
+							type="email"
+							name="email"
+							onChange={handleChange}
+							placeholder="Enter your email address"
+						/>
+						<label className="password-label">Password</label>
+						<input
+							className="details-input"
+							type="password"
+							name="password"
+							onChange={handleChange}
+							placeholder="·······"
+						/>
+						<section className="options-section">
+							<div>
+								<input
+									className="remember-input"
+									type="checkbox"
+									onChange={handlePersistance}
+								/>
+								<label className="remember-label">Remember for 30 Days</label>
+							</div>
+							<p onClick={forgotPasswordHandler}>Forgot password</p>
+						</section>
+						<button onClick={handleSubmit} className="sign-btn">
+							Sign in
+						</button>
+						<button onClick={handleGoogle} className="google-btn">
+							<img src={google} alt="" />
+							Sign in with google
+						</button>
+						<p className="question">
+							Don't have an account? <Link to="/signup">Sign up for free</Link>
+						</p>
+						<img src={vector} className="vector" alt="" />
+					</form>
+				</div>
+			</div>
+			<div className="image-half">
+				<img src={Main} alt="" className="landing-image" />
+			</div>
+		</div>
+	);
 }
--- src/Components/Signup/Signup.js
+++ src/Components/Signup/Signup.js
@@ -14,126 +14,130 @@ import Main from "../../images/Intro_img.svg";
 import "./signup.css";
 
 export default function Signup() {
-  const { signup, setDisplayName, signInGoogle, createUserDetails } = useAuth();
-  const navigate = useNavigate();
-  const [loading, setLoading] = useState(false);
-  const [data, setData] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
-  const [errors, setErrors] = useState({
-    name: "",
-    email: "",
-    password: "",
-  });
+	const { signup, setDisplayName, signInGoogle, createUserDetails } = useAuth();
+	const navigate = useNavigate();
+	const [loading, setLoading] = useState(false);
+	const [data, setData] = useState({
+		name: "",
+		email: "",
+		password: "",
+	});
+	const [errors, setErrors] = useState({
+		name: "",
+		email: "",
+		password: "",
+	});
 
-  const handleChange = e => {
-    const { name, value } = e.target;
-    setData(prev => ({
-      ...prev,
-      [name]: value,
-    }));
-  };
+	const handleChange = (e) => {
+		const { name, value } = e.target;
+		setData((prev) => ({
+			...prev,
+			[name]: value,
+		}));
+	};
 
-  //Create a user, set a display name, create user details
-  const handleSubmit = async e => {
-    e.preventDefault();
-    if (Object.keys(errors).length === 0) {
-      try {
-        setLoading(true);
-        const { user } = await signup(data.email, data.password);
-        await setDisplayName(user, data.name);
-        await createUserDetails(user.uid, data.name, data.email);
-        navigate("/", { replace: true });
-      } catch (err) {
-        setErrors({ name: "Failed to create an account" });
-      }
-    }
-    setLoading(false);
-  };
+	//Create a user, set a display name, create user details
+	const handleSubmit = async (e) => {
+		e.preventDefault();
+		if (Object.keys(errors).length === 0) {
+			try {
+				setLoading(true);
+				const { user } = await signup(data.email, data.password);
+				await setDisplayName(user, data.name);
+				await createUserDetails(user.uid, data.name, data.email);
+				navigate("/", { replace: true });
+			} catch (err) {
+				setErrors({ name: "Failed to create an account" });
+			}
+		}
+		setLoading(false);
+	};
 
-  //Check for errors and set Errors
-  useEffect(() => {
-    setErrors({});
-    if (data.password.length < 6 && data.password !== "") {
-      setErrors(prev => ({
-        ...prev,
-        password: "Password is too short",
-      }));
-    }
-    if (!/.+@.+\..+/.test(data.email) && data.email !== "") {
-      setErrors(prev => ({
-        ...prev,
-        email: "Email is incorrect",
-      }));
-    }
-    if (!/^[a-zA-Z\s]+$/g.test(data.name) && data.name !== "") {
-      setErrors(prev => ({
-        ...prev,
-        name: "Name can contain only letters",
-      }));
-    }
-  }, [data]);
+	//Check for errors and set Errors
+	useEffect(() => {
+		setErrors({});
+		if (data.password.length < 6 && data.password !== "") {
+			setErrors((prev) => ({
+				...prev,
+				password: "Password is too short",
+			}));
+		}
+		if (!/.+@.+\..+/.test(data.email) && data.email !== "") {
+			setErrors((prev) => ({
+				...prev,
+				email: "Email is incorrect",
+			}));
+		}
+		if (!/^[a-zA-Z\s]+$/g.test(data.name) && data.name !== "") {
+			setErrors((prev) => ({
+				...prev,
+				name: "Name can contain only letters",
+			}));
+		}
+	}, [data]);
 
-  // function handleGoogle(e){
-  //     e.preventDefault();
-  //             console.log(userSign.email,userSign.password)
-  //     signInWithPopup(auth, provider)
-  //     .then((cred)=>{
-  //         console.log(cred);
-  //     })
-  //     .catch((err) => { console.log(err) })
-  // }
+	// function handleGoogle(e){
+	//     e.preventDefault();
+	//             console.log(userSign.email,userSign.password)
+	//     signInWithPopup(auth, provider)
+	//     .then((cred)=>{
+	//         console.log(cred);
+	//     })
+	//     .catch((err) => { console.log(err) })
+	// }
 
-  return (
-    <div className="intro-container">
-      <div className="form-container">
-        <img src={Logo} className="logo" alt="" />
-        <h1 className="title-sign">Create a new account</h1>
-        <p className="details">Welcome back! Please enter your details</p>
-        <form className="form-signup" onSubmit={handleSubmit}>
-          <label className="name-label">Full Name</label>
-          <input
-            className="details-input"
-            type="text"
-            name="name"
-            onChange={handleChange}
-            value={data.name}
-            placeholder="Enter your full name"
-          />
-          <label className="email-label">Email</label>
-          <input
-            className="details-input"
-            type="email"
-            name="email"
-            onChange={handleChange}
-            value={data.email}
-            placeholder="Enter your email address"
-          />
-          <label className="password-label">Password</label>
-          <input
-            className="details-input"
-            type="password"
-            name="password"
-            onChange={handleChange}
-            value={data.password}
-            placeholder="·······"
-          />
-          <button onClick={handleSubmit} className="sign-btn">
-            Sign up
-          </button>
-          <button className="google-btn">
-            <img src={google} alt="" />
-            Sign up with google
-          </button>
-          <p className="question">
-            Already have an account? <Link to="/signin">Sign in</Link>
-          </p>
-          <img className="vector" src={vector} alt="" />
-        </form>
-      </div>
-      <img src={Main} alt="" />
-    </div>
-  );
+	return (
+		<div className="su-intro-container">
+			<div className="su-form-half">
+				<div className="su-form-container">
+					<img src={Logo} className="su-logo" alt="" />
+					<h1 className="su-title-sign">Create a new account.</h1>
+					<p className="su-details">Please enter your details.</p>
+					<form className="su-form-signup" onSubmit={handleSubmit}>
+						<label className="su-name-label">Full Name</label>
+						<input
+							className="su-details-input"
+							type="text"
+							name="name"
+							onChange={handleChange}
+							value={data.name}
+							placeholder="Enter your full name"
+						/>
+						<label className="su-email-label">Email</label>
+						<input
+							className="su-details-input"
+							type="email"
+							name="email"
+							onChange={handleChange}
+							value={data.email}
+							placeholder="Enter your email address"
+						/>
+						<label className="su-password-label">Password</label>
+						<input
+							className="su-details-input"
+							type="password"
+							name="password"
+							onChange={handleChange}
+							value={data.password}
+							placeholder="·······"
+						/>
+						<button onClick={handleSubmit} className="su-sign-btn">
+							Sign up
+						</button>
+						<button className="su-google-btn">
+							<img src={google} alt="" />
+							Sign up with google
+						</button>
+						<p className="question">
+							Already have an account? <Link to="/signin">Sign in</Link>
+						</p>
+						<img className="su-vector" src={vector} alt="" />
+					</form>
+				</div>
+			</div>
+			<div className="su-image-half">
+				<img src={Main} alt="" className="su-landing-image" />
+			</div>
+		</div>
+	);
 }
