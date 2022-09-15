f6f82aa376d0a1b3e97282bf3700cccf2d86a73a
Edmndsn <edmondson.jackw@gmail.com>
2022-09-15 19:31:39 +0100

    changed to props

--- src/Components/Expenses/ExpenseCard.js
+++ src/Components/Expenses/ExpenseCard.js
@@ -5,51 +5,51 @@ import "./CreateExpense.css";
 import { serverTimestamp } from "firebase/firestore";
 
 function ExpenseCard(props) {
-  const currentDate = new Date().toISOString().substring(0, 10);
+  // const currentDate = new Date().toISOString().substring(0, 10);
 
-  const [data, setData] = useState({
-    title: props.title || "",
-    company: props.company || "",
-    currency: props.currency || "",
-    amount: props.amount || "",
-    category: props.category || "",
-    date: props.date || currentDate,
-    invoice: props.invoice,
-    createdAt: serverTimestamp(),
-    recurring: props.recurring || false,
-  });
+  // const [data, setData] = useState({
+  //   title: props.title || "",
+  //   company: props.company || "",
+  //   currency: props.currency || "",
+  //   amount: props.amount || "",
+  //   category: props.category || "",
+  //   date: props.date || currentDate,
+  //   invoice: props.invoice,
+  //   createdAt: serverTimestamp(),
+  //   recurring: props.recurring || false,
+  // });
 
-  const [disableSubmit, setDisableSubmit] = useState(false);
+  // const [disableSubmit, setDisableSubmit] = useState(false);
 
-  function handleChange(event) {
-    const { name, value, checked, type } = event.target;
+  // function handleChange(event) {
+  //   const { name, value, checked, type } = event.target;
 
-    setData(prev => ({
-      ...prev,
-      [name]: type === "checkbox" ? checked : value,
-    }));
-  }
+  //   setData(prev => ({
+  //     ...prev,
+  //     [name]: type === "checkbox" ? checked : value,
+  //   }));
+  // }
 
-  useEffect(() => {
-    checkForErrors();
-  }, [data]);
+  // useEffect(() => {
+  //   checkForErrors();
+  // }, [data]);
 
-  function handleSubmit(event) {
-    event.preventDefault();
-    const error = checkForErrors();
-    if (!error) props.handleClick(data);
-  }
+  // function handleSubmit(event) {
+  //   event.preventDefault();
+  //   const error = checkForErrors();
+  //   if (!error) props.handleClick(data);
+  // }
 
-  function checkForErrors() {
-    for (let item in data) {
-      if (data[item] === "") {
-        setDisableSubmit(true);
-        return true;
-      } else {
-        setDisableSubmit(false);
-      }
-    }
-  }
+  // function checkForErrors() {
+  //   for (let item in data) {
+  //     if (data[item] === "") {
+  //       setDisableSubmit(true);
+  //       return true;
+  //     } else {
+  //       setDisableSubmit(false);
+  //     }
+  //   }
+  // }
 
   return (
     <div
@@ -58,16 +58,16 @@ function ExpenseCard(props) {
       <div className={props.class}>
         {props.title && (
           <div className="card-element name-business-container">
-            <p className="expense-card-text business">{data.title}</p>
-            <p className="expense-card-subtext company">{data.company}</p>
+            <p className="expense-card-text business">{props.title}</p>
+            <p className="expense-card-subtext company">{props.company}</p>
           </div>
         )}
         {props.category && (
           <div className="card-element category">
             <div className="category-image-container">
-              <CategoryImage key={props.id} expenseCategory={data.category} />
+              <CategoryImage key={props.id} expenseCategory={props.category} />
             </div>
-            <p className="expense-card-text category">{data.category}</p>
+            <p className="expense-card-text category">{props.category}</p>
           </div>
         )}
         {props.amount && props.recurringExpenses ? (
@@ -80,14 +80,14 @@ function ExpenseCard(props) {
         ) : props.amount ? (
           <div className="card-element amount">
             <span className="selected-currency">{props.currency}</span>
-            <p className="expense-card-text amount">{data.amount}</p>
+            <p className="expense-card-text amount">{props.amount}</p>
           </div>
         ) : (
           ""
         )}
         {props.date && (
           <div className="card-element date-container">
-            <p className="expense-card-text date">{data.date}</p>
+            <p className="expense-card-text date">{props.date}</p>
           </div>
         )}
         {props.recurringExpense && (
@@ -97,13 +97,13 @@ function ExpenseCard(props) {
               disabled
               type="checkbox"
               name="recurring"
-              checked={data.recurring}
+              checked={props.recurring}
             ></input>
           </div>
         )}
         {props.invoice && (
           <div className="card-element invoice">
-            <p className="expense-card-text invoice">{data.invoice}</p>
+            <p className="expense-card-text invoice">{props.invoice}</p>
           </div>
         )}
         {props.edit && (
@@ -111,7 +111,7 @@ function ExpenseCard(props) {
             <button
               type="button"
               className="action-button"
-              onClick={event => props.edit(event, data.invoice)}
+              onClick={event => props.edit(event, props.invoice)}
             >
               Edit
             </button>
